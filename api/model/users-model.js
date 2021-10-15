838075f6e82f0a81eb58e029ac238963e76569d8
ehsan2793 <ehsan2793@gmail.com>
2021-10-14 18:09:17 -0700

    added more dependency

---
+++ api/model/users-model.js
@@ -0,0 +1,18 @@
+const db = require('../../data/dbConfig');
+
+const getAll = () => {
+    return "hello"
+};
+const getById = async (id) => {
+    return "hello"
+};
+const insert = async (user) => {
+
+    return "hello"
+};
+
+module.exports = {
+    getAll,
+    getById,
+    insert,
+};
