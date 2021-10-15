743da822b2b38f4a6f2c5eb6bdc81ecef5953427
ehsan2793 <ehsan2793@gmail.com>
2021-10-14 18:20:16 -0700

    getAll and getById test has been created

--- api/model/users-model.test.js
+++ api/model/users-model.test.js
@@ -19,6 +19,26 @@ it('test env should be set', () => {
     expect(process.env.NODE_ENV).toBe('testing')
 })
 
-// describe('',() => {
+describe('getAll users model function', () => {
 
-// })
+    beforeEach(async () => {
+        await db('users').insert({ username: 'ehsan', password: '1234' })
+    })
+    it('getAll shoud return at leat have one user', async () => {
+        const users = await Us.getAll()
+        expect(users).toHaveLength(1)
+    })
+})
+
+
+describe('getById users model function', () => {
+    let id
+    beforeEach(async () => {
+        id = await db('users').insert({ username: 'ehsan', password: '1234' })
+    })
+    it('getById shoud return user with specified id', async () => {
+        const user = await Us.getById(id)
+        expect(user).toMatchObject({ username: 'ehsan', password: '1234' })
+    })
+})
+
